e83c977457e065b8f55c099b5076485341289596
John Allsopp <john@westciv.com>
2014-06-14 10:57:54 +1000

    speaker more info

---
+++ js/helpers.js
@@ -0,0 +1,41 @@
+var helpers = {
+	
+	init: function(){
+		var speakerHeroElements = document.querySelectorAll(".speaker-hero")
+		var lightBox = document.querySelector("#lightbox")
+		for (var i=0; i < speakerHeroElements.length; i++) {
+			speakerHeroElements[i].addEventListener("click", helpers.showSpeakerInfo, false)
+		};
+		
+		lightBox.addEventListener('click', helpers.hideSpeakerInfo, false)
+		
+	},
+	
+	showSpeakerInfo: function(event){
+		var element = event.currentTarget;
+		var moreElement = element.querySelector(".speaker-more")
+
+		var lightBox = document.querySelector("#lightbox")
+		lightBox.classList.add("visible")
+
+		
+		if (moreElement) {
+			moreElement.classList.add("visible") 	
+		}
+	},
+	
+	hideSpeakerInfo: function(){
+		var visibleSpeakerElements = document.querySelectorAll(".speaker-more.visible")
+		
+		for (var i=0; i < visibleSpeakerElements.length; i++) {
+			visibleSpeakerElements[i].classList.remove("visible")
+		};
+		
+		var lightBox = document.querySelector("#lightbox")
+		lightBox.classList.remove("visible")
+		
+	}
+	
+}
+
+window.addEventListener('load', helpers.init, false)
